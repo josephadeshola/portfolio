d70a24a5aff71c988dad70225b757dd4604fcc24
Adeshola Ayomide <josephay125d@gmail.com>
2024-12-27 04:44:02 +0100

    navbar toggle

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { MdLightMode } from "react-icons/md";
-import { MdDarkMode } from "react-icons/md";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
 import { useDarkMode } from '../../DarkModeContext';
 
 const Navbar = () => {
-    const { isDarkMode, toggleDarkMode } = useDarkMode();
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleToggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
 
   return (
     <div>
-        <nav className="border-gray-200 dark:bg-gray-900">
+      <nav className="border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="https://flowbite.com/"
@@ -21,11 +25,11 @@ const Navbar = () => {
           </a>
 
           <button
-            data-collapse-toggle="navbar-default"
+            onClick={handleToggleDropdown}
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded={isDropdownOpen}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -44,8 +48,14 @@ const Navbar = () => {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
+          <div
+            className={`w-full md:block md:w-auto ${
+              isDropdownOpen ? 'block' : 'hidden'
+            }`}
+            id="navbar-default"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link
                   to="/"
@@ -58,7 +68,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/project"
-                  className="block py-2 px-3  hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="block py-2 px-3 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Project
                 </Link>
@@ -71,7 +81,6 @@ const Navbar = () => {
                   Resume
                 </a>
               </li>
-              
               <li>
                 <Link
                   to="/contact"
@@ -93,7 +102,7 @@ const Navbar = () => {
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
