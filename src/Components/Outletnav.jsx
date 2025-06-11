e4dde952cf1a3ef2db7196d4b8f2094696fbc6e0
Adeshola Ayomide <josephay125d@gmail.com>
2025-06-11 06:44:48 +0100

    code update

--- src/Components/Outletnav.jsx
+++ src/Components/Outletnav.jsx
@@ -11,23 +11,16 @@ const Outletnav = () => {
 
   return (
     <div className="relative min-h-screen">
-      {/* Background image */}
       <div
         className="absolute inset-0 bg-center bg-cover z-0"
         style={{
-          // backgroundImage:
-          //   "url('https://img.freepik.com/free-photo/dark-room-with-light-background_1409-1809.jpg?uid=R159996107&ga=GA1.1.1567084357.1721659617&semt=ais_items_boosted&w=740')",
           backgroundImage:
-            "url('https://img.freepik.com/free-vector/realistic-neon-lights-background_23-2148907367.jpg?uid=R159996107&ga=GA1.1.1567084357.1721659617&semt=ais_items_boosted&w=740')",
+            "url('https://img.freepik.com/free-photo/colorful-abstract-nebula-space-background_53876-111356.jpg?uid=R159996107&ga=GA1.1.1567084357.1721659617&semt=ais_items_boosted&w=740')",
           backgroundRepeat: "no-repeat", 
           backgroundSize: "cover",     
         }}
       />
-
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-black/90 z-10" />
-
-      {/* Main Content */}
+      <div className="absolute inset-0 bg-black/80 z-10" />
       <div className={isDarkMode ? "relative z-20 text-white" : "relative z-20 bg-white text-black"}>
         <Navbar onClick={toggleDarkMode} />
         <Home />
